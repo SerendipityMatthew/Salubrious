a3e15f4d960c7e270a0bb3ff091c4af044f21ed1
Matthew <xuwanjin1991@Gmail.com>
2021-04-01 22:55:44 +0800

    first commit, project Salubrious
    Signed-off-by: Matthew <xuwanjin1991@Gmail.com>

---
+++ Salubrious/Salubrious/SalubriousApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalubriousApp.swift
+//  Salubrious
+//
+//  Created by Matthew on 2021/4/1.
+//
+
+import SwiftUI
+
+@main
+struct SalubriousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
